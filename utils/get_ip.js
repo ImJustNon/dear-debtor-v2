5422956344965ac7727bc4e37a052c4327367df6
ImJustNon <Im_Just_Non@hotmail.com>
2023-03-15 22:08:23 +0700

    Fix find ip

--- utils/get_ip.js
+++ utils/get_ip.js
@@ -1,8 +1,7 @@
-const axios = require('axios');
 
 module.exports = {
-    get_ip: async(req) =>{
-        let request = await axios.get("https://api64.ipify.org/?format=json");  // (req.headers['x-forwarded-for'] || req.connection.remoteAddress || '').split(',')[0].trim();
-        return request.data.ip;
+    get_ip: (req) =>{
+        let ip = (req.headers['x-forwarded-for'] || req.connection.remoteAddress || '').split(',')[0].trim();
+        return ip;
     },
 }
