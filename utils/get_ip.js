6c3934e451069ba57fa720ac5f688093d0f15605
ImJustNon <Im_Just_Non@hotmail.com>
2023-03-15 22:03:03 +0700

    Fix ip api

--- utils/get_ip.js
+++ utils/get_ip.js
@@ -1,6 +1,8 @@
+const axios = require('axios');
+
 module.exports = {
-    get_ip: (req) =>{
-        let ip = (req.headers['x-forwarded-for'] || req.connection.remoteAddress || '').split(',')[0].trim();
-        return ip;
+    get_ip: async(req) =>{
+        let request = await axios.get("https://api64.ipify.org/?format=json");  // (req.headers['x-forwarded-for'] || req.connection.remoteAddress || '').split(',')[0].trim();
+        return request.data.ip;
     },
 }
